1416c69b99f5d77b17b4c4b5faf1241cfb30d57a
Brad Murray <bmurray@fitbit.com>
2018-04-07 19:22:07 -0400

    Still doesn't work

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const Telegraf = require('telegraf')
+const Telegraf = require('telegraf');
+const { reply } = Telegraf;
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.telegram.getMe().then((botInfo) => {
-  bot.options.username = botInfo.username
+  bot.options.username = botInfo.username;
 })
 
-bot.hears('yankees', ({ reply }, next) => reply('/fucktheyankees'));
+//bot.hears(/.*yankees.*/, reply('/fucktheyankees'));
 
-bot.startPolling()
+bot.on('message', (ctx, next) => {
+    if (ctx.message.text.indexOf("yankees") != -1) {
+      ctx.telegram.sendMessage(ctx.message.chat.id, "/fucktheyankees");
+    }
+});
+
+
+bot.startPolling();
