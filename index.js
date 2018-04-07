6aae1a4b5d303e9650a43523e28277dec38bf060
Brad Murray <bmurray@fitbit.com>
2018-04-07 18:57:50 -0400

    Grab username

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const Telegraf = require('telegraf')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
+bot.telegram.getMe().then((botInfo) => {
+  bot.options.username = botInfo.username
+})
+
 bot.hears('yankees', ({ reply }, next) => reply('/fucktheyankees'));
 
 bot.startPolling()
